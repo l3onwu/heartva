d5fa09c4adfa03f7a95516725eefcef133c6005a
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2023-05-01 19:23:29 +1000

    Horizontal hrgraph page layout

--- src/views/HRGraphPage.tsx
+++ src/views/HRGraphPage.tsx
@@ -5,25 +5,30 @@ import HRGraph from "../components/HRGraph";
 const HRGraphPage = () => {
   return (
     <Box width="100%">
-      <Flex width="100%" justify="space-between">
-        {/* Left side */}
-        <Box
-          width="50%"
-          height="calc(100vh - 55px)"
-          overflow="scroll"
-          mr="20px"
-          pb="20px"
-        >
-          <ActivityList />
-        </Box>
-
+      <Flex
+        width="100%"
+        justify="space-between"
+        flexWrap="wrap"
+        maxH="calc(100vh - 80px)"
+      >
         {/* Right side */}
-        <Box width="50%" height="calc(100vh - 80px)" pr="40px">
+        <Box width="100%" height="300px" px="40px" mb="20px">
           {/* Page heading */}
           {/* <Text color="#9C88AA" fontWeight="bold" fontSize="30px" mb="30px">
             HR Graph
           </Text> */}
           <HRGraph />
+        </Box>
+
+        {/* Left side */}
+        <Box
+          height="calc(100vh - 380px)"
+          width="100%"
+          overflow="scroll"
+          pb="20px"
+          borderTop="0.5px solid #333333"
+        >
+          <ActivityList />
         </Box>
       </Flex>
     </Box>
