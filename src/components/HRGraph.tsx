85fe20014ffcb1ab03c3b2c439921782d454670b
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2023-06-19 18:44:01 +1000

    Feature time-based graph

--- src/components/HRGraph.tsx
+++ src/components/HRGraph.tsx
@@ -1,105 +1,137 @@
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
 } from "chart.js";
-import {Line} from "react-chartjs-2";
-import {useGlobalContext} from "../lib/context";
+import "chartjs-adapter-luxon";
+
+import { Line } from "react-chartjs-2";
+import { useGlobalContext } from "../lib/context";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
 );
 
 export default function HRGraph() {
-    const {userHook} = useGlobalContext();
+  const { userHook } = useGlobalContext();
 
-    let x = userHook?.activities
-        ?.map((act) => {
-            return (
-                (Math.round((act?.moving_time / (act?.distance / 1000) / 60) * 100) /
-                    100) *
-                20
-            );
-        })
-        ?.reverse();
+  let paceData = userHook?.activities
+    ?.filter((act) => {
+      return (
+        new Date(act?.start_date_local).getFullYear() === userHook?.statsYear
+      );
+    })
+    .map((act) => {
+      return (
+        (Math.round((act?.moving_time / (act?.distance / 1000) / 60) * 100) /
+          100) *
+        20
+      );
+    })
+    ?.reverse();
 
-    let y = userHook?.activities
-        ?.map((act) => {
-            return act?.average_heartrate;
-        })
-        ?.reverse();
+  let y = userHook?.activities
+    ?.filter((act) => {
+      return (
+        new Date(act?.start_date_local).getFullYear() === userHook?.statsYear
+      );
+    })
+    ?.map((act) => {
+      return act?.average_heartrate;
+    })
+    ?.reverse();
 
-    const options = {
-        tension: 0.3,
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            x: {
-                // display: false,
-                grid: {
-                    // color: "#222222",
-                    lineWidth: 0,
-                    color: "transparent",
-                },
-                ticks: {
-                    display: false,
-                }
-            },
-            y: {
-                grid: {
-                    color: "#222222",
-                    lineWidth: 1,
-                    // color: "transparent",
-                },
-            },
+  let dateLabels = userHook?.activities
+    ?.filter((act) => {
+      return (
+        new Date(act?.start_date_local).getFullYear() === userHook?.statsYear
+      );
+    })
+    ?.map((act) => {
+      return new Date(act?.start_date_local);
+    });
+
+  let hrData = userHook?.activities
+    ?.filter((act) => {
+      return (
+        new Date(act?.start_date_local).getFullYear() === userHook?.statsYear
+      );
+    })
+    ?.map((act) => {
+      return act?.average_heartrate;
+    });
+
+  const options = {
+    // tension: 0.3,
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        type: "time" as const,
+        time: {
+          unit: "month" as const,
+          displayFormats: {
+            month: "MMM" as const,
+          },
         },
-        plugins: {
-            legend: {
-                display: false,
-                position: "bottom" as const,
-            },
-            // title: {
-            //   display: true,
-            //   text: "Chart.js Line Chart",
-            // },
+        min: new Date(userHook?.statsYear, 0, 1, 0, 0, 0),
+        max: new Date(userHook?.statsYear, 11, 30, 23, 59, 59),
+      },
+      y: {
+        grid: {
+          color: "#222222",
+          lineWidth: 1,
+          // color: "transparent",
         },
-    };
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+        position: "bottom" as const,
+      },
+      // title: {
+      //   display: true,
+      //   text: "Chart.js Line Chart",
+      // },
+    },
+  };
 
-    const data = {
-        labels: userHook?.activities
-            ?.map((act) => {
-                return act?.distance;
-            })
-            ?.reverse(),
-        datasets: [
-            {
-                label: "Heartrate",
-                data: y,
-                borderColor: "rgb(255, 99, 132)",
-                backgroundColor: "rgba(255, 99, 132, 0.5)",
-                pointRadius: 1,
+  const data = {
+    labels: dateLabels,
+    datasets: [
+      {
+        label: "HR",
+        borderColor: "rgb(255, 0, 0)",
+        data: hrData,
+        fill: false,
+        pointRadius: 0,
+      },
+      {
+        label: "Pace",
+        borderColor: "rgb(0, 0, 255)",
+        data: paceData,
+        fill: false,
+        pointRadius: 0,
+      },
+    ],
+  };
 
-            },
-            {
-                label: "Pace",
-                data: x,
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                pointRadius: 1,
-            },
-        ],
-    };
-
-    return <Line options={options} data={data}/>;
+  {
+    // @ts-ignore
+    return <Line options={options} data={data} />;
+  }
 }
--- src/views/HRGraphPage.tsx
+++ src/views/HRGraphPage.tsx
@@ -1,74 +1,117 @@
-import {Flex, Box, Text, Button, Stack, Select} from "@chakra-ui/react";
+import { Flex, Box, Text, Button, Stack, Select } from "@chakra-ui/react";
 import ActivityList from "../components/ActivityList";
 import HRGraph from "../components/HRGraph";
+import { useGlobalContext } from "../lib/context";
 
 const HRGraphPage = () => {
-    return (
-        <Box width="100%">
+  const { userHook } = useGlobalContext();
 
-            <Box
-                // border={"0.5px solid #986C8D"} borderRadius={"5px"}
-                mb="20px"
+  return (
+    <Box width="100%">
+      <Box
+        // border={"0.5px solid #986C8D"} borderRadius={"5px"}
+        mb="20px"
+      >
+        {/*Page heading */}
+        <Flex justify="space-between" mb="10px">
+          <Flex direction={"row"} align={"center"}>
+            <Text
+              fontSize="18px"
+              color="white"
+              fontWeight={"semibold"}
+              padding={"0px"}
+              mr={"7px"}
             >
-                {/*Page heading */}
-                <Flex
-                    justify="space-between"
-                    mb="10px"
-                >
-                    <Text fontSize="18px" color="white" fontWeight={"semibold"}>Trends for 2023</Text>
-
-                    {/*Filters*/}
-                    <Stack direction={"row"} spacing={2}>
-                        <Select size="xs" variant='outline' defaultValue='date' color={"#444444"}
-                                borderColor={"#444444"}>
-                            <option value='date'>Date</option>
-                        </Select>
-                        <Select size="xs" variant='outline' defaultValue='distance' color={"#444444"}
-                                borderColor={"#444444"}>
-                            <option value='distance'>Distance</option>
-                            <option value='speed'>Speed</option>
-                            <option value='heartrate'>Heartrate</option>
-                        </Select>
-                        <Select size="xs" variant='outline' defaultValue='-' color={"#444444"} borderColor={"#444444"}>
-                            <option value='-'>-</option>
-                            <option value='distance'>Distance</option>
-                            <option value='speed'>Speed</option>
-                            <option value='heartrate'>Heartrate</option>
-                        </Select>
-
-                        {/*<Button size={"xs"} variant={"outline"} colorScheme={"grey"}>Year</Button>*/}
-                    </Stack>
-                </Flex>
-
-
-                {/* HR Graph */}
-                <Box height="220px" width={"100%"}
-                     mb={"00px"}
-                    // borderBottom={"0.5px solid #986C8D"}
-                    //  p={"10px"}
-                    // border={"0.5px solid #333333"} borderRadius={"5px"} padding={"8px"}
-                >
-                    <HRGraph/>
-                </Box>
-            </Box>
-
-
-            <Text fontSize="18px" color="white" fontWeight={"semibold"} mb="10px"
-            >Activities</Text>
-
-            {/* HR Table */}
-            <Box
-                height="calc(100vh - 400px)"
-                width="100%"
-                overflow="scroll"
-                pb="20px"
-                px="5px"
+              Trends for
+            </Text>
+            <Select
+              size={"xs"}
+              fontSize="14px"
+              color="white"
+              fontWeight={"semibold"}
+              defaultValue={2023}
+              width={"fit-content"}
+              borderRadius={"5px"}
+              padding={"0px"}
+              borderColor={"#444444"}
+              onChange={(e) => {
+                userHook?.setStatsYear(parseInt(e.target.value));
+              }}
             >
+              <option value={2023}>2023</option>
+              <option value={2022}>2022</option>
+              <option value={2018}>2018</option>
+            </Select>
+          </Flex>
 
-                <ActivityList/>
-            </Box>
+          {/*Filters*/}
+          <Stack direction={"row"} spacing={2}>
+            <Select
+              size="xs"
+              variant="outline"
+              defaultValue="date"
+              color={"#444444"}
+              borderColor={"#444444"}
+            >
+              <option value="date">Date</option>
+            </Select>
+            <Select
+              size="xs"
+              variant="outline"
+              defaultValue="distance"
+              color={"#444444"}
+              borderColor={"#444444"}
+            >
+              <option value="distance">Distance</option>
+              <option value="speed">Speed</option>
+              <option value="heartrate">Heartrate</option>
+            </Select>
+            <Select
+              size="xs"
+              variant="outline"
+              defaultValue="-"
+              color={"#444444"}
+              borderColor={"#444444"}
+            >
+              <option value="-">-</option>
+              <option value="distance">Distance</option>
+              <option value="speed">Speed</option>
+              <option value="heartrate">Heartrate</option>
+            </Select>
+
+            {/*<Button size={"xs"} variant={"outline"} colorScheme={"grey"}>Year</Button>*/}
+          </Stack>
+        </Flex>
+
+        {/* HR Graph */}
+        <Box
+          height="220px"
+          width={"100%"}
+          mb={"00px"}
+          // borderBottom={"0.5px solid #986C8D"}
+          //  p={"10px"}
+          // border={"0.5px solid #333333"} borderRadius={"5px"} padding={"8px"}
+        >
+          <HRGraph />
         </Box>
-    );
+      </Box>
+
+      <Text fontSize="18px" color="white" fontWeight={"semibold"} mb="10px">
+        Activities
+      </Text>
+
+      {/* HR Table */}
+      <Box
+        height="calc(100vh - 400px)"
+        width="100%"
+        overflow="scroll"
+        pb="20px"
+        px="5px"
+      >
+        <ActivityList />
+      </Box>
+    </Box>
+  );
 };
 
 export default HRGraphPage;
